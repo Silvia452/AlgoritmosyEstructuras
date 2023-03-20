34854e822d5d886dfe7faa84b7da4d14642427eb
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-20 11:57:29 +0100

    Nuevos cambios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,25 @@ public class Main {
         polinomio.cargarTermino(2, 3);
         polinomio.cargarTermino(3, 2);
         polinomio.cargarTermino(4, 1);
+        polinomio.cargarTermino(5, 1);
+        polinomio.cargarTermino(1, 0);
+        polinomio.mostrarContenido();
+
+        polinomio.modificarTermino(-1, 3);
+        polinomio.eliminarTermino(1);
+        polinomio.mostrarContenido();
+
+        PolinomioImplement otroPolinomio = new PolinomioImplement();
+        otroPolinomio.cargarTermino(2, 3);
+        otroPolinomio.cargarTermino(-1, 1);
+        otroPolinomio.cargarTermino(3, 0);
+        otroPolinomio.mostrarContenido();
+
+        Polinomio resultado = polinomio.sumar(otroPolinomio);
+        resultado.mostrarContenido();
+
+        resultado = polinomio.multiplicar(otroPolinomio);
+        resultado.mostrarContenido();
 
 
     }
